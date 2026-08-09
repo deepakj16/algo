a5349d5b6ef347a66f0a2e1ad9dad04b859a1f23
deepakj16 <deepak.jayabalan@gmail.com>
2026-08-09 14:20:58 -0700

    Create 373. Find K Pairs with Smallest Sums.py

---
+++ Heap/373. Find K Pairs with Smallest Sums.py
@@ -0,0 +1,63 @@
+"""
+373. Find K Pairs with Smallest Sums
+Attempted
+Medium
+Topics
+premium lock icon
+Companies
+You are given two integer arrays nums1 and nums2 sorted in non-decreasing order and an integer k.
+
+Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+
+Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+Output: [[1,2],[1,4],[1,6]]
+Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+Example 2:
+
+Input: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+Output: [[1,1],[1,1]]
+Explanation: The first 2 pairs are returned from the sequence: [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 105
+-109 <= nums1[i], nums2[i] <= 109
+nums1 and nums2 both are sorted in non-decreasing order.
+1 <= k <= 104
+k <= nums1.length * nums2.length
+
+"""
+
+
+class Solution(object):
+    def kSmallestPairs(self, nums1, nums2, k):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        h =[]
+        result = []
+        heapq.heappush(h, (nums1[0] + nums2[0], 0, 0))
+        while h and len(result)<k:
+            s, i, j = heapq.heappop(h)
+            heapq.heappush(result,[nums1[i],nums2[j]])
+            if len(result)!=k:
+                if i+1<=k and j+1<=k and nums1[i+1] + nums2[j] <= nums1[i] + nums2[j+1]:
+                    heapq.heappush(h,([nums1[i+1],nums2[j]], i+1, j))
+                else:
+                    heapq.heappush(h,([nums1[i],nums2[j+1]], i, j+1 ))
+                print('while if')
+                print(h)
+            else:
+                break
+        return result
+
