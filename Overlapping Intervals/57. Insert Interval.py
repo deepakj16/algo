a1e39c0857e492f2349dd18192cbbf1318b32934
deepakj16 <deepak.jayabalan@gmail.com>
2026-08-09 15:11:42 -0700

    Create 57. Insert Interval.py

---
+++ Overlapping Intervals/57. Insert Interval.py
@@ -0,0 +1,93 @@
+"""
+57. Insert Interval
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+Two intervals are considered overlapping if they share at least one point.
+
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+Return intervals after the insertion.
+
+Note that you don't need to modify intervals in-place. You can make a new array and return it.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+Output: [[1,5],[6,9]]
+Example 2:
+
+Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+Output: [[1,2],[3,10],[12,16]]
+Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+ 
+
+Constraints:
+
+0 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti <= endi <= 105
+intervals is sorted by starti in ascending order.
+newInterval.length == 2
+0 <= start <= end <= 105
+"""
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        op = []
+        i = 0
+        while i < len(intervals) and intervals[i][1] < newInterval[0]:
+            op.append(intervals[i])
+            i+=1
+
+        while i<len(intervals) and newInterval[1] >= intervals[i][0]:
+            newInterval = [min(newInterval[0],intervals[i][0]), max(newInterval[1],intervals[i][1])]
+            print(newInterval)
+            i+=1
+        op.append(newInterval)
+
+        while i<len(intervals):
+            op.append(intervals[i])
+            i+=1
+
+        return op
+
+
+
+
+        '''
+        intervals.append(newInterval)
+        intervals = sorted(intervals, key=lambda x:x[0])
+
+        op = [intervals[0]]
+        j=0
+        for i in range(1,len(intervals)):
+            print('before if')
+            print(intervals[i])
+            print(op)
+            if op[j][1] >= intervals[i][0]:
+                op[j][1] = max(intervals[i][1], op[j][1])
+                print(' if')
+                print(op)
+            else:
+                op.append(intervals[i])
+                j += 1
+                print('else')
+                print(op)
+        return op
+        '''
+
+       
+        
