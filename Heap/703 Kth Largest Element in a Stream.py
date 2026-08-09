6af662ba336af582fc417f0d63e17e99e7161180
deepakj16 <deepak.jayabalan@gmail.com>
2026-08-09 13:35:07 -0700

    Create 703 Kth Largest Element in a Stream.py

---
+++ Heap/703 Kth Largest Element in a Stream.py
@@ -0,0 +1,97 @@
+"""
+Easy
+Topics
+premium lock icon
+Companies
+You are part of a university admissions office and need to keep track of the kth highest test score from applicants in real-time. This helps to determine cut-off marks for interviews and admissions dynamically as new applicants submit their scores.
+
+You are tasked to implement a class which, for a given integer k, maintains a stream of test scores and continuously returns the kth highest test score after a new score has been submitted. More specifically, we are looking for the kth highest score in the sorted list of all scores.
+
+Implement the KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of test scores nums.
+int add(int val) Adds a new test score val to the stream and returns the element representing the kth largest element in the pool of test scores so far.
+ 
+
+Example 1:
+
+Input:
+["KthLargest", "add", "add", "add", "add", "add"]
+[[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+
+Output: [null, 4, 5, 5, 8, 8]
+
+Explanation:
+
+KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+kthLargest.add(3); // return 4
+kthLargest.add(5); // return 5
+kthLargest.add(10); // return 5
+kthLargest.add(9); // return 8
+kthLargest.add(4); // return 8
+
+Example 2:
+
+Input:
+["KthLargest", "add", "add", "add", "add"]
+[[4, [7, 7, 7, 7, 8, 3]], [2], [10], [9], [9]]
+
+Output: [null, 7, 7, 7, 8]
+
+Explanation:
+
+KthLargest kthLargest = new KthLargest(4, [7, 7, 7, 7, 8, 3]);
+kthLargest.add(2); // return 7
+kthLargest.add(10); // return 7
+kthLargest.add(9); // return 7
+kthLargest.add(9); // return 8
+
+"""
+
+class KthLargest(object):
+    def __init__(self, k, nums):
+        self.h = []
+        for i in nums:
+            heapq.heappush(self.h,i)
+            if len(self.h)>k:
+                heapq.heappop(self.h)
+        print(self.h)
+
+    def add(self, val):
+        heapq.heappush(self.h, val)
+        heapq.heappop(self.h)
+        return self.h[0]
+
+    '''''
+
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        self.h = []
+
+        for i in nums:
+            heapq.heappush(self.h, i)
+            if len(self.h)>k:
+                heapq.heappop(self.h)
+        
+        
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+        heapq.heappush(self.h, val)
+        heapq.heappop(self.h)
+
+        return self.h[0]
+    """
+    '''''
+
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
